d64991ebfbf3ac34871093742ebeed30584a27cc
bmloredo <brunosams1@outlook.com>
2022-03-07 20:29:21 -0300

    Aula presencial1

---
+++ Aula Presencial 1/Arrays.js
@@ -0,0 +1,28 @@
+const bruno = {
+  bruno_Ar1: [
+    {
+      nome: "Marcos",
+      idade: 15,
+      masculino: true
+    },
+    {
+      nome: "Eve",
+      idade: 26,
+      masculino: false
+    },
+    {
+      nome: "Bruno",
+      idade: 26,
+      masculino: true
+    },
+  ],
+  bruno_Ar2: [
+    {
+      feijao: "Sim",
+      arroz: "Não"
+    }
+  ]
+};
+
+
+
