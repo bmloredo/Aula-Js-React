2577a5c693cffbeb95987609e34b6acadc5dff23
bmloredo <brunosams1@outlook.com>
2022-03-16 17:38:34 -0300

    Estrutura de dados montadada

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,63 +2,70 @@
 import "./App.css";
 
 function App() {
-  const pessoas = [
-    {
-      nome: "Bruno",
-      idade: 26,
-      dtNasc: "20/05/1995",
-      nomeMae: "Sandra",
-      nomePai: "Edmo"
-    },
-    {
-      nome: "Cassio",
-      idade: 38,
-      dtNasc: "05/04/1987",
-      nomeMae: "Lucia Vera",
-      nomePai: ""
-    },
-    {
-      nome: "Eduardo",
-      idade: 39,
-      dtNasc: "13/05/1983",
-      nomeMae: "Marcia",
-      nomePai: "Adelmo"
-    },
-    {
-      nome: "Fatima",
-      idade: 21,
-      dtNasc: "08/02/2001",
-      nomeMae: "Cicera",
-      nomePai: "Valdemir",
-    },
-    {
-      nome: "Ellen",
-      idade: 38,
-      dtNasc: "08/09/1983",
-      nomeMae: "Marta",
-      nomePai: ""
-    },
-  ];
 
-  return (
-    <div className="App">
-      {
-        pessoas.map(pessoa => {
-          return (
-            <div className="principal">
-              <div className="pessoa"> 
-                <p>Nome: {pessoa.nome}</p>
-                <p>idade: {pessoa.idade}</p>
-                <p>Data de Nascimento: {pessoa.dtNasc}</p>
-                <p>Nome da Mãe: {pessoa.nomeMae}</p>
-                {pessoa.nomePai && <p>Nome do Pai: {pessoa.nomePai}</p>}
-              </div>
-            </div>
-          )
-        })
-        }
-    </div>
-  );
+const livros = [
+  {
+        nome: 'Ego Transformado',
+        autor: 'Timothy Keller',
+        qt_paginas: 48,
+        bestSeller: false,
+  },
+  {
+        nome: 'Até que nada mais importe',
+        autor: 'Luciano Subirá',
+        qt_paginas: 154,
+        bestSeller: true,
+  },
+  {
+    nome: 'Discipulado',
+    autor: 'Dietrich Bonhoeffer',
+    qt_paginas: 253,
+    bestSeller: false,
+  },
+  {
+    nome: 'Cristianismo Puro e Simples',
+    autor: 'C.S Lewis',
+    qt_paginas: 288,
+    bestSeller: true
+  }
+]
+
+const venda = [
+  {
+    vendedor: 'Marcos',
+    frutaVendida: 'abacaxi',
+    loja: 'Loja 1'
+  },
+  {
+    vendedor: 'João',
+    frutaVendida: 'abacate',
+    loja: 'Loja 2'
+  },
+  {
+    vendedor: 'Pedro',
+    frutaVendida: 'abacaxi',
+    loja: 'Loja 3'
+  },
+]
+
+//Variáveis Filter
+
+const best = livros.filter(book => {
+  return book.bestSeller === false
+})
+
+const fruit = venda.filter(sellerFruit=>{
+  return sellerFruit.frutaVendida === 'abacaxi'
+})
+
+    return (
+      <div>
+        {best[0].autor}
+        <br></br>
+        {best[1].autor}
+        
+      </div>
+    );
 }
 
 export default App;
