3ff5e65c3e35a083ba1c8b631b6b16fd4a980d69
bmloredo <brunosams1@outlook.com>
2022-03-25 16:04:21 -0300

    onclick e onChange

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,102 +1,58 @@
 //import logo from './logo.svg';
 import "./App.css";
+import React, { useRef } from 'react';
 
 function App() {
-  const livros = [
-    {
-      nome: "Ego Transformado",
-      autor: "Timothy Keller",
-      qt_paginas: 48,
-      bestSeller: false,
-    },
-    {
-      nome: "Até que nada mais importe",
-      autor: "Luciano Subirá",
-      qt_paginas: 154,
-      bestSeller: true,
-    },
-    {
-      nome: "Discipulado",
-      autor: "Dietrich Bonhoeffer",
-      qt_paginas: 253,
-      bestSeller: false,
-    },
-    {
-      nome: "Cristianismo Puro e Simples",
-      autor: "C.S Lewis",
-      qt_paginas: 288,
-      bestSeller: true,
-    },
-  ];
-
-  const venda = [
-    {
-      vendedor: "Marcos",
-      frutaVendida: "Abacaxi",
-      loja: "Loja 1",
-    },
-    {
-      vendedor: "João",
-      frutaVendida: "Abacate",
-      loja: "Loja 2",
-    },
-    {
-      vendedor: "Pedro",
-      frutaVendida: "Abacaxi",
-      loja: "Loja 3",
-    },
-  ];
-
-  //Variáveis Filter
-
-  const best = livros.filter((livrosb) => {
-    return livrosb.bestSeller === false;
-  });
-
-  const fruta = venda.filter((vendaFruta) => {
-    return vendaFruta.frutaVendida === "Abacaxi";
-  });
-
+  const number = 4
+  const message = "Você conseguiu!"
+  
+  
   return (
     <div className="App">
-      <div className="livro-best">
-        <div className="title-livros">
-          <h1>Livros que não são Best Sellers</h1>
-        </div>
-        <div className="principal">
-        {best.map((livroBestSeller) => {
-          return (
-              <div className="Livros">
-                <p>Nome do Livro: {livroBestSeller.nome}</p>
-                <p>Autor: {livroBestSeller.autor}</p>
-                <p>Quantidade de Páginas: {livroBestSeller.qt_paginas}</p>
-              </div>
-            
-          );
-        })}
-        </div>
-      </div>
-
-      <div className="venda-abacaxi">
-        <div className="title-vendas">
-          <h1>Venda de Abacaxi</h1>
-        </div>
-        <div  className="abacaxi">
-        {fruta.map((frutaAbacaxi) => {
-          return (
-            <div>
-              <p>Nome do Vendedor: {frutaAbacaxi.vendedor}</p>
-              <p>Fruta Vendida: {frutaAbacaxi.frutaVendida}</p>
-              <p>Loja: {frutaAbacaxi.loja}</p>
-            </div>
-          );
-        })}
-        </div>
-      </div>
+      <label>Digite o número aqui:</label>
+      <input maxlength="1" className="input" name="firstName" onChange={(e) => {alert(JSON.parse(e.target.value)+number)}} /> 
+      <div className="message" onClick={() => {alert(message)}}>Clique aqui</div>
+      
     </div>
+    
+    
   );
+
 }
 
 export default App;
 
 //return string[0].toUpperCase() + string.slice(1);
+//<input ref={containerRef} maxlength="1" className="input" name="firstName" onChange={myChange} /> 
+
+/*
+const containerRef = useRef()
+  
+  
+  const myChange = (e) => {
+    const texto = e.target.value
+    if (texto.length === 2 ){
+        alert(`A soma do número digitado: ${texto} Com ${number} é: ${JSON.parse(texto) + number}` )
+    } else if (texto.length < 2){
+      console.log(containerRef.current.value.length)
+    }else {
+      //console.log(containerRef.current.value ='')
+      console.log(containerRef.current.value.length)
+    }
+    
+  }
+
+
+
+<label>Digite o número aqui:</label>
+      <input ref={containerRef} className="input" name="firstName" onChange={myChange} /> 
+      
+      {  
+      
+        <div>       
+          <p>Se você quer somar apenas 1 número, clique no botão ao lado</p>
+          <button>Some Aqui{containerRef.current}</button>
+        </div>
+       
+      }
+*/
